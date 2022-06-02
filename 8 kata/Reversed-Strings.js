0bd5eb21064ff0b7c341a71eab0d8a0620fdb073
shevchukigor <musictweaker@gmail.com>
2022-06-02 21:29:36 +0300

    reversed strings

---
+++ 8 kata/Reversed-Strings.js
@@ -0,0 +1,15 @@
+/*
+Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+*/
+
+// my solution
+function solution(str) {
+  return str.split("").reverse().join("");
+}
+// best practices
+function solution(str) {
+  return str.split("").reverse().join("");
+}
