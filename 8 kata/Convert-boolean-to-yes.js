9562c6877dd68f7a07672d4c14eaafce27e13f46
shevchukigor <musictweaker@gmail.com>
2022-06-03 08:24:27 +0300

    boolean to yes no

---
+++ 8 kata/Convert-boolean-to-yes.js
@@ -0,0 +1,15 @@
+/*
+Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+
+*/
+
+// my solution
+function boolToWord(bool) {
+  return bool ? "Yes" : "No";
+}
+
+// best practices
+function boolToWord(bool) {
+  return bool ? "Yes" : "No";
+}
