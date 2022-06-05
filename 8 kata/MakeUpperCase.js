7e4b800ced003165a9747a50fcd509fbbd50718f
shevchukigor <musictweaker@gmail.com>
2022-06-05 12:35:29 +0300

    make upper case

---
+++ 8 kata/MakeUpperCase.js
@@ -0,0 +1,10 @@
+/*
+Write a function which converts the input string to uppercase.
+*/
+
+// my solution
+function makeUpperCase(str) {
+  return str.toUpperCase();
+}
+// best practices
+const makeUpperCase = (str) => str.toUpperCase();
