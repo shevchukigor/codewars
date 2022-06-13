898035532ca465385fb205e72de42013b13d980c
shevchukigor <musictweaker@gmail.com>
2022-06-13 17:39:16 +0300

    return negative

---
+++ 8 kata/Return-Negative.js
@@ -0,0 +1,31 @@
+/*
+In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+Examples
+makeNegative(1);    // return -1
+makeNegative(-5);   // return -5
+makeNegative(0);    // return 0
+makeNegative(0.12); // return -0.12
+Notes
+The number can be negative already, in which case no change is required.
+Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
+*/
+
+// my solution
+function makeNegative(num) {
+  if (num > 0) {
+    return -num;
+  } else {
+    return num;
+  }
+}
+// best practices
+
+function makeNegative(num) {
+  return -Math.abs(num);
+}
+
+///
+function makeNegative(num) {
+  return num < 0 ? num : -num;
+}
