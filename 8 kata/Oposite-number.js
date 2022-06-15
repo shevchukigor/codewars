65ee87002774b1523af316574928b12dc8cbbb09
shevchukigor <musictweaker@gmail.com>
2022-06-15 14:36:36 +0300

    oposite number

---
+++ 8 kata/Oposite-number.js
@@ -0,0 +1,15 @@
+/*
+Very simple, given an integer or a floating-point number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+*/
+
+// my solution
+function opposite(number) {
+  return -number;
+}
+// best practices
